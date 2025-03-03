d4d5dac7e29ff892bb322661690634068850582a
Rishabh Singh <140408377+rishabhx01@users.noreply.github.com>
2025-03-03 18:00:26 +0000

    skeleton of cpp

---
+++ 01_basics/01_introduction.cpp
@@ -0,0 +1,34 @@
+// Some basic stuff about cpp
+
+// Skeleton of cpp
+
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    cout << "hello rishabh, keep moving forward" << endl;
+    return 0;
+}
+
+
+// #include <iostream> ... in order to take an input or output we use <iostream> and
+// in order to use this library we use #include.
+
+// using namespace std; ... by using this we do not need to write
+// std in each line. like std::cout << "hello rishabh, keep moving forward" << endl;
+
+// cout << .. it is used for printing output.
+
+// cin >> .. it is used for taking input from user.
+
+// endl; or "\n" .. takes you to the next line but \n is faster.
+
+
+// use #include<bits/stdc++.h> ... it includes all the library available in cpp.
+
+
+/* 
+Another thing that always appear in a C++ program is int main(). 
+This is called a function. Any code inside its curly brackets {} will be executed.
+*/
